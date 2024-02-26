5c001661e83d8d8510e109c65d08c21a35d62391
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-02-26 20:09:41 +0800

    add `/questions`
    - return a json response of the `Question` struct
    - add the Question struct from previous chapters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,63 @@
+use serde::{Deserialize, Serialize};
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path("hello").map(|| format!("Hello, World!"));
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3000)).await;
+    let routes = get_items;
+
+    println!("->> Listening on port: 3000\n");
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
+}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    Ok(warp::reply::json(&question))
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct QuestionId(String);
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Self {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No ID provided")),
+        }
+    }
 }
